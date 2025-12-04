482973346bfd99c568b1f62c38c8117baee0f7d1
Valer1712 <dreptatevaler1712@gmail.com>
2025-12-04 09:50:28 +0100

    commentaire algo du spread

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,38 @@ import type { Task } from "./types/TaskType";
 function App() {
 
 const [tasks, setTasks] = useState<Task[]>([]);
-
+//on enlève l'id de la tâche vu qu'il est pas présent à cette étape de la création
 function handleAddTask(newTask: Omit<Task, "id">) {
-    const id = tasks.length + 1; // version simple et lisible
-    setTasks([...tasks, { id, ...newTask }]);
+  //on ajoute +1 à l'index du tableau Task, ce qui permet de commencer à 1 au lieu de 0 qui est vide
+  //on utilise l'index pour créer l'id de la tâche
+    const id = tasks.length + 1;//on parcours le tableau d'objet par l'id de la tâche
+    setTasks([...tasks, { id, ...newTask }]);//on reprends l'ancien tableau, et on ajoute une nouvelle tâche.
 }
 
+/* handleAddtaks est équivalent à : 
+const newArray = [];
+for (let i = 0; i < prev.length; i++) {
+  newArray.push(prev[i]);
+}
+
+const newTaskObj = {
+  id: prev.length + 1,
+  title: newTask.title,
+  description: newTask.description,
+};
+
+newArray.push(newTaskObj);
+
+setTasks(newArray);
+
+...prev
+
+→ dépose tous les éléments de prev dans un nouveau tableau.
+
+...newTask
+
+→ dépose toutes les propriétés de l’objet newTask dans un nouvel objet.
+*/
 
   return (
     <BrowserRouter>
--- src/types/TaskType.tsx
+++ src/types/TaskType.tsx
@@ -1,5 +1,5 @@
 export interface Task {
   id: number;
   title: string;
-  description?: string;
+  description?: string;//le ? sert à "nullifié, ce qui rend description non nécessaire.
 }
